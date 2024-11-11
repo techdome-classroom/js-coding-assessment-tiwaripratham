fe3cc7c2cf5ec23ec60c14d88b2467301c1346b3
tiwaripratham <tiwaripratham195@gmail.com>
2024-11-11 14:12:07 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -5,7 +5,9 @@ const getTotalIsles = function (grid) {
   const cols = grid[0].length;
   let islandCount = 0;
 
+  // Helper function for DFS traversal
   function dfs(row, col) {
+    // Check boundaries and water cells
     if (
       row < 0 ||
       col < 0 ||
@@ -15,24 +17,28 @@ const getTotalIsles = function (grid) {
     )
       return;
 
+    // Mark cell as visited by changing it to 'W'
     grid[row][col] = "W";
 
-    dfs(row + 1, col);
-    dfs(row - 1, col);
-    dfs(row, col + 1);
-    dfs(row, col - 1);
+    // Traverse all four directions
+    dfs(row + 1, col); // down
+    dfs(row - 1, col); // up
+    dfs(row, col + 1); // right
+    dfs(row, col - 1); // left
   }
 
+  // Loop through each cell in the grid
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
+      // If we find an unvisited 'L', it starts a new island
       if (grid[i][j] === "L") {
         islandCount++;
-        dfs(i, j);
+        dfs(i, j); // Mark all parts of the island
       }
     }
+  }
 
-    return islandCount;
-  };
-}
+  return islandCount;
+};
 
 module.exports = getTotalIsles;
